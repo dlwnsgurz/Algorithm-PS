67c066f7557dd490df46b0ccd058d50d08075eea
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2022-07-06 21:08:28 +0900

    20220706

---
+++ 2206.cpp
@@ -0,0 +1,8 @@
+//
+//  2206.cpp
+//  Baekjoon
+//
+//  Created by LEE on 2022/07/06.
+//
+
+#include "2206.hpp"
---
+++ 6593.cpp
@@ -0,0 +1,8 @@
+//
+//  6593.cpp
+//  Baekjoon
+//
+//  Created by LEE on 2022/07/06.
+//
+
+#include "6593.hpp"
