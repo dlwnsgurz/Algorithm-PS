fa1821bfc0f515c1187bb2b56f3ca631d8f7ad91
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2022-07-13 18:05:12 +0900

    20220713

---
+++ 1992.cpp
@@ -0,0 +1,8 @@
+//
+//  1992.cpp
+//  Baekjoon
+//
+//  Created by LEE on 2022/07/13.
+//
+
+#include "1992.hpp"
---
+++ 2630.cpp
@@ -0,0 +1,8 @@
+//
+//  2630.cpp
+//  Baekjoon
+//
+//  Created by LEE on 2022/07/13.
+//
+
+#include "2630.hpp"
