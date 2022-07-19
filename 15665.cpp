6f95e2bec41f2a6f3b262a959c24749abc62f589
dlwnsgurz <ljhlmkljhlmkljhlmk@naver.com>
2022-07-19 19:48:04 +0900

    15666

---
+++ 15665.cpp
@@ -0,0 +1,8 @@
+//
+//  15665.cpp
+//  Baekjoon
+//
+//  Created by LEE on 2022/07/19.
+//
+
+#include "15665.hpp"
---
+++ 15666.cpp
@@ -0,0 +1,8 @@
+//
+//  15666.cpp
+//  Baekjoon
+//
+//  Created by LEE on 2022/07/19.
+//
+
+#include "15666.hpp"
